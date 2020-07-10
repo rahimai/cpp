cf73a497abfb69b755e586883104659f256f9710
rahimai <lawanrahim.ai@gmail.com>
2020-07-10 17:17:37 -0400

    rotation function

--- tetris.cpp
+++ tetris.cpp
@@ -22,43 +22,46 @@ int rotate(int px, int py, int r){
     return 0;
 }
 
+bool DoesPieceFit(int nTetromino, int nRotation, int nPosX, int nPosY)
+{
+	// All Field cells >0 are occupied
+	for (int px = 0; px < 4; px++)
+		for (int py = 0; py < 4; py++)
+		{
+			// Get index into piece
+			int pi = Rotate(px, py, nRotation);
+
+			// Get index into field
+			int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+
+			// Check that test is in bounds. Note out of bounds does
+			// not necessarily mean a fail, as the long vertical piece
+			// can have cells that lie outside the boundary, so we'll
+			// just ignore them
+			if (nPosX + px >= 0 && nPosX + px < nFieldWidth)
+			{
+				if (nPosY + py >= 0 && nPosY + py < nFieldHeight)
+				{
+					// In Bounds so do collision check
+					if (tetromino[nTetromino][pi] != L'.' && pField[fi] != 0)
+						return false; // fail on first hit
+				}
+			}
+		}
+
+	return true;
+}
+
 int main(){
 
-    //Create assets 
-    tetromino[0].append("..X.");
-    tetromino[0].append("..X.");
-    tetromino[0].append("..X.");
-    tetromino[0].append("..X.");
-
-    tetromino[1].append("..X.");
-    tetromino[1].append(".XX.");
-    tetromino[1].append(".X..");
-    tetromino[1].append("....");
-
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
-    tetromino[1].append("..X.");
+    //Create assetsk 
+	tetromino[0].append("..X...X...X...X."); 
+	tetromino[1].append("..X..XX...X.....");
+	tetromino[2].append(".....XX..XX.....");
+	tetromino[3].append("..X..XX..X......");
+	tetromino[4].append(".X...XX...X.....");
+	tetromino[5].append(".X...X...XX.....");
+	tetromino[6].append("..X...X..XX.....");
 
 
 
