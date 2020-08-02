f7eb04487b83ba228442809938fffe39d05852bf
rahimai <lawanrahim.ai@gmail.com>
2020-08-02 10:48:10 -0400

    linkedlist1

---
+++ linkedlist.cpp
@@ -0,0 +1,25 @@
+//Write a function that counts the number of times a given int occurs in a Linked List 
+#include <iostream>
+
+using namespace std;
+
+class List{
+
+    private: 
+    int data;
+    List* next; 
+
+};
+
+
+int main(){
+
+    int i_key;
+
+    cout << "Enter integer to be searched for: " << endl;
+    cin >> i_key;
+
+    List head = new List();
+    List second = new List();
+    
+}
